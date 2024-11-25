b41b8d1e044d85ecd1906a38a73214265db9e242
zhentian-wan <answer881215@gmail.com>
2024-11-25 21:53:22 +0200

    router.push methods

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,8 +1,19 @@
 <script setup>
+import { useRouter } from "vue-router";
 import { defineProps } from "vue";
 
 defineProps(["event"]);
+const router = useRouter();
+
+const register = () => {
+  // Call to API
+  // If registered then redirect to event details
+  router.push({
+    name: "event-details",
+  });
+};
 </script>
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me</button>
 </template>
